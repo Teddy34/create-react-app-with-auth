e4225f4cd5f26a382ec34ddf5e161d6fe05883e3
Jordi Sicart <jordi.sicart@realimpactanalytics.com>
2020-04-09 09:38:13 +0200

    Setup server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((response) => response.json())
+      .then((hello) => console.log(hello))
+      .catch((err) => console.error('### Error:', err));
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
